899e8ab02e12b1c532a86ff570351ed715588a78
Yaswanth MLVSS <ymlvss@gmail.com>
2026-02-01 22:22:12 +0530

    Updated run.sh file

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-# set -e
-# cd "$(dirname "$0")"
+set -e
+cd "$(dirname "$0")"
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 curl -fsSL https://ollama.ai/install.sh | sh
 ollama pull llama3.2:1b
-python run_api.py
+python run_api.py &
+sleep 3
+python run_chatbot_web.py
